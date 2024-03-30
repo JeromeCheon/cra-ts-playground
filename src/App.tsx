8daa188c2c2803ffddfac4e8139fb141d6954746
JeromeCheon <cheonjh4572@gmail.com>
2024-03-30 12:17:21 +0900

    Update Counter -> Todo with basic state setting

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import './App.css';
 import styled from '@emotion/styled';
 import { Title } from './components/Title';
 import { TodoList } from './components/TodoList';
+import {useState} from "react";
 
 const Container = styled.div`
   height: 100vh;
@@ -13,10 +14,16 @@ const Container = styled.div`
 `;
 
 function App() {
+  const [toDoList, setToDoList] = useState(['리액트 공부하기', '운동하기', '책읽기'])
+
+  const onDelete = (todo: string) => {
+    setToDoList(toDoList.filter(item => item !== todo))
+  }
+
   return (
     <Container>
       <Title label="할 일 목록" />
-      <TodoList toDoList={['리액트 공부하기', '운동하기', '책읽기']} />
+      <TodoList toDoList={toDoList} onDelete={onDelete}/>
     </Container>
   );
 }
