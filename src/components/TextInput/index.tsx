5e5d481b4a12c3c60e8bc7b0009700a84c772bcd
JeromeCheon <cheonjh4572@gmail.com>
2024-03-30 12:38:22 +0900

    Update todo with textinput

---
+++ src/components/TextInput/index.tsx
@@ -0,0 +1,14 @@
+import styled from '@emotion/styled';
+
+export const Input = styled.input`
+  font-size: 1.2rem;
+  padding: 8px;
+`;
+interface Props {
+  readonly value: string;
+  readonly onChange: (text: string) => void;
+}
+
+export const TextInput = ({ value, onChange }: Props) => {
+  return <Input value={value} onChange={(event) => onChange(event.target.value)} />;
+};
