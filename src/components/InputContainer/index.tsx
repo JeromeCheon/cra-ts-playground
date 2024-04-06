2526543aec7f6974586ca89d71c9101893ea8805
JeromeCheon <cheonjh4572@gmail.com>
2024-04-06 12:39:07 +0900

    Update components with contextAPI

--- src/components/InputContainer/index.tsx
+++ src/components/InputContainer/index.tsx
@@ -2,19 +2,14 @@ import { ShowInputButton } from 'components/ShowInputButton';
 import { ToDoInput } from 'components/TodoInput';
 import { useState } from 'react';
 
-interface Props {
-  readonly onAdd: (toDo: string) => void;
-}
-
-export const InputContainer = ({ onAdd }: Props) => {
+export const InputContainer = () => {
   const [showTodoInput, setShowTodoInput] = useState(false);
-  const onAddTodo = (todo: string) => {
-    onAdd(todo);
+  const onClose = () => {
     setShowTodoInput(false);
   };
   return (
     <>
-      {showTodoInput && <ToDoInput onAdd={onAddTodo} />}
+      {showTodoInput && <ToDoInput onClose={onClose} />}
       <ShowInputButton show={showTodoInput} onClick={() => setShowTodoInput(!showTodoInput)} />
     </>
   );
